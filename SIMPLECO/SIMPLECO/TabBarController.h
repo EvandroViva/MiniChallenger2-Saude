85a353d1810f51df77ba754347d6c8dfd1e2bd78
Liliane Bezerra Lima <liliane.b.lima@outlook.com>
2015-03-26 16:24:07 -0300

    MUDANÇA NO STORYBOARD

---
+++ SIMPLECO/SIMPLECO/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  SIMPLECO
+//
+//  Created by Liliane Bezerra Lima on 26/03/15.
+//  Copyright (c) 2015 Liliane Bezerra Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
