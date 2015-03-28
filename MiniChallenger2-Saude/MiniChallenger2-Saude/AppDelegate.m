283f44fe7893e54929bc8ed671dc9ea36c425c80
Humberto Luiz Julião Brandão <humbertojuliao3@gmail.com>
2015-03-28 16:43:51 -0300

    DB no programa, faltam as queries

--- MiniChallenger2-Saude/MiniChallenger2-Saude/AppDelegate.m
+++ MiniChallenger2-Saude/MiniChallenger2-Saude/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"p6Wi9pBFOKt6WoG3z2DtEoCvoQmSC7GveFLwrlMW"
+                  clientKey:@"tDcXoLC7iDVpXNq7pLMhzJjQuL4N492UMM28GWfE"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     // Override point for customization after application launch.
     return YES;
 }
