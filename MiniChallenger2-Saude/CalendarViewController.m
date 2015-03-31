d8bf0203948d3b888a444f06400a81099817f92d
Liliane Bezerra Lima <liliane.b.lima@outlook.com>
2015-03-31 15:59:47 -0300

    Inclusa do calendário

--- MiniChallenger2-Saude/CalendarViewController.m
+++ MiniChallenger2-Saude/CalendarViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "CalendarViewController.h"
+#import "JTCalendar/JTCalendar.h"
+#import "JTCalendar.h"
 
 @interface CalendarViewController ()
 
@@ -14,24 +16,84 @@
 
 @implementation CalendarViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.calendar = [JTCalendar new];
+    
+    
+    // All modifications on calendarAppearance have to be done before setMenuMonthsView and setContentView
+    // Or you will have to call reloadAppearance
+    {
+        self.calendar.calendarAppearance.calendar.firstWeekday = 2; // Sunday == 1, Saturday == 7
+        self.calendar.calendarAppearance.dayCircleRatio = 9. / 10.;
+        self.calendar.calendarAppearance.ratioContentMenu = 1.;
+    }
+    
+    [self.calendar setMenuMonthsView:self.calendarMenuView];
+    [self.calendar setContentView:self.calendarContentView];
+    [self.calendar setDataSource:self];
+    
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self transitionExample];
+    [self.calendar reloadData];
+}
+
+#pragma mark - JTCalendarDataSource
+
+- (BOOL)calendarHaveEvent:(JTCalendar *)calendar date:(NSDate *)date
+{
+    return (rand() % 10) == 1;
+}
+
+- (void)calendarDidDateSelected:(JTCalendar *)calendar date:(NSDate *)date
+{
+    NSLog(@"Date: %@", date);
+}
+
+#pragma mark - Transition examples
+
+- (void)transitionExample
+{
+    CGFloat newHeight = 300;
+    if(self.calendar.calendarAppearance.isWeekMode){
+        newHeight = 75.;
+    }
+    
+    [UIView animateWithDuration:.5
+                     animations:^{
+                         self.calendarContentViewHeight.constant = newHeight;
+                         [self.view layoutIfNeeded];
+                     }];
+    
+    [UIView animateWithDuration:.25
+                     animations:^{
+                         self.calendarContentView.layer.opacity = 0;
+                     }
+                     completion:^(BOOL finished) {
+                         [self.calendar reloadAppearance];
+                         
+                         [UIView animateWithDuration:.25
+                                          animations:^{
+                                              self.calendarContentView.layer.opacity = 1;
+                                          }];
+                     }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)Hoje:(id)sender {
+    [self.calendar setCurrentDate:[NSDate date]];
 }
-*/
 
+- (IBAction)Visualizacao:(id)sender {
+    self.calendar.calendarAppearance.isWeekMode = !self.calendar.calendarAppearance.isWeekMode;
+    
+    [self transitionExample];
+}
 @end
